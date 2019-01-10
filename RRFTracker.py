801adc0197f8862db2f7b15f2ee32e4d8d3c6190
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-01-10 05:41:09 +0100

    Fix Temp bug

--- RRFTracker.py
+++ RRFTracker.py
@@ -298,13 +298,13 @@ def main(argv):
         elif(blanc_alternate == 4):         # Thermal monitor
             if board == 'orangepi':
                 tmp = os.popen("cat /sys/devices/virtual/thermal/thermal_zone0/temp").readline()
-                tmp = int(tmp)
+                tmp = str(tmp) + 'C'
             else:
                 tmp = os.popen("vcgencmd measure_temp").readline()
                 tmp = re.findall('\d+\.\d+', tmp)
-                tmp = tmp[0]
+                tmp = str(tmp[0])
 
-            line[4] = 'Spotnik Temp ' + str(tmp) + ' C'
+            line[4] = 'Spotnik Temp ' + tmp
 
             blanc_alternate = 0
 
