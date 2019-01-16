32adb7a890527c8933a02617bba9f73c5987f174
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-01-16 13:35:37 +0100

    Add extended log and best history

--- RRFTracker.py
+++ RRFTracker.py
@@ -173,10 +173,12 @@ def main(argv):
 
     letter = {'C': 16, 'E': 17, 'F': 12, 'N': 15, 'O': 13, 'R': 11, 'T': 14}
 
-    call = ['F4HWN', 'RRFTracker', '']
+    call = ['F4HWN', 'RRFTracker', '', '', '', '', '', '', '', '']
+    #call = ['22 F4GGU', '75 F5ZEQ', '79 F1ZOT', '75 F5ZEQ', '75 F5ZEQ', '79 F1ZOT', '', '', '', '']
     call_current = call[0]
     call_previous = call[1]
-    call_time = 'Waiting TX'
+    call_time = ['Waiting TX', '', '', '', '', '', '', '', '', '']
+    #call_time = ['22:37:56', '22:38:07', '22:39:15', '22:39:15', '22:39:15', '22:38:07', '', '', '', '']
 
     blanc = True
     blanc_alternate = 0
@@ -209,6 +211,9 @@ def main(argv):
     timestamp_start = time.time()
 
     history = dict()
+
+    #history = {'75 F4HWN': 7, '75 F1GWX': 2, '79 F1ZTO': 4, '22 F4GGU': 17, '78 F4GLU': 23}
+
     line = [None] * 7
 
     # Check board
@@ -226,10 +231,12 @@ def main(argv):
         # If midnight...
 
         tmp = datetime.datetime.now()
-        now = tmp.strftime('%H:%M')
+        now = tmp.strftime('%H:%M:%S')
         hour = int(tmp.strftime('%H'))
+        minute = int(now[3:-3])
+        seconde = int(now[-2:])
 
-        if(now == '00:00'):
+        if(now[:5] == '00:00'):
             qso_total += qso
             qso = 0
             for q in xrange(0, 24):         # Clean histogram
@@ -266,9 +273,13 @@ def main(argv):
 
             if (call_previous != call_current):
                 call_previous = call_current
-                call[2] = call[1]
-                call[1] = call[0]
+
+                for i in xrange(9, 0, -1):
+                    call[i] = call[i - 1]
+                    call_time[i] = call_time[i - 1]
+
                 call[0] = call_current
+
                 history = save_stat(history, call[1])
                 qso += 1
             else:
@@ -279,14 +290,13 @@ def main(argv):
 
             # Format call time
 
-            call_time = 'Last TX '
             tmp = datetime.datetime.now()
             now = tmp.strftime('%H:%M:%S')
             hour = int(tmp.strftime('%H'))
 
             qso_hour[hour] = qso - sum(qso_hour[:hour])
 
-            call_time += now
+            call_time[0] = now
 
             line[0] = call[2]
             line[1] = call[1]
@@ -304,7 +314,10 @@ def main(argv):
 
             line[0] = call[1]
             line[1] = call[0]
-            line[2] = call_time
+            if qso == 0:
+                line[2] = call_time[0]
+            else:
+                line[2] = 'Last TX ' + call_time[0]
 
         if(blanc_alternate == 0):           # TX today
             tmp = 'TX Today '
@@ -354,83 +367,148 @@ def main(argv):
         icon = ImageFont.truetype('fonts/fontello.ttf', 14)                     # Icon font
                   
         with canvas(device) as draw:
-            if device.height == 64:     # Only if 128 x 64 pixels
-                for i in xrange(0, 128, 2):
-                    draw.point((i, 25), fill = 'white')
-                    draw.point((i, 40), fill = 'white')
-                    draw.text((0,26), u'\ue801', font = icon, fill = 'white')   # Icon stat
 
-            
-            if wake_up is True:
-                draw.text((2, 0), u'\uf130', font = icon, fill = 'white')       # Icon talk
-            
-            if line[2][:4] == 'Last':                                           # Icon clock (DIY...)
-                x = 6
-                y = 17
-                draw.ellipse((x - 6, y - 6, x + 6, y + 6), outline = 'white')
-                draw.line((x, y, x + 2, y + 2), fill = 'white')
-                draw.line((x, y, x, y - 3), fill = 'white')
+            if wake_up is False and device.height == 64:                        # If sleep
+                if 'Waiting' not in call_time[0] and minute % 2 == 0:           # History log extended
 
-            # Print data
+                    draw.rectangle((0, 0, 127, 63), fill = 'black')
 
-            i = 0
+                    for i in xrange(0, 128, 2):
+                        draw.point((i, 10), fill = 'white')
 
-            for l in line:
-                if l is not None:
-                    w, h = draw.textsize(text = l, font = font)
+                    w, h = draw.textsize(text = room + ' Last TX', font = font)
                     tab = (device.width - w) / 2
-                    vide = ' ' * 22             # Hack to speed clear screen line... 
-                    draw.text((0, i), vide, font = font, fill = 'white')
-                    draw.text((tab, i), l, font = font, fill = 'white')
-                    i += h 
-                    if i == 24:
-                        if device.height != 64:  # Break if 128 x 32 pixels
+                    draw.text((tab, 0), room + ' Last TX', font = font, fill = 'white')
+
+                    i = 16
+                    
+                    for j in xrange(0, 5):
+                        if 'Waiting' in call_time[j]:
                             break
-                        i += 6
+
+                        draw.rectangle((0, i - 1, 42, i + 7), fill = 'white')
+                        draw.line((43, i, 43, i + 6), fill = 'white')
+                        draw.line((44, i + 2, 44, i + 4), fill = 'white')
+                        draw.point((45, i + 3), fill = 'white')
+
+                        draw.text((1, i), call_time[j], font = font, fill = 'black')
+                        draw.text((54, i), call[j], font = font, fill = 'white')
+
+                        i += 10
+
+                elif len(history) >= 5 and minute % 1 == 0:                     # Best log extended
+
+                    draw.rectangle((0, 0, 127, 63), fill = 'black')
+                    for i in xrange(0, 128, 2):
+                        draw.point((i, 10), fill = 'white')
+
+                    w, h = draw.textsize(text = room + ' Best TX', font = font)
+                    tab = (device.width - w) / 2
+                    draw.text((tab, 0), room + ' Best TX', font = font, fill = 'white')
+
+                    tmp = sorted(history.items(), key=lambda x: x[1])
+                    tmp.reverse();
+                    
+                    best_min = min(history, key=history.get)
+                    best_max = max(history, key=history.get)
+
+                    i = 16
+
+                    for j in xrange(0, 5):
+                        c, n = tmp[j]
+                        t = interpolation(n, history[best_min], history[best_max], 12, 42)
+                        n = str(n)
+
+                        draw.rectangle((0, i - 1, t, i + 7), fill = 'white')
+                        draw.line((t + 1, i, t + 1, i + 6), fill = 'white')
+                        draw.line((t + 2, i + 2, t + 2, i + 4), fill = 'white')
+                        draw.point((t + 3, i + 3), fill = 'white')
+
+                        draw.text((1, i), n, font = font, fill = 'black')
+                        draw.text((54, i), c, font = font, fill = 'white')
+
+                        i += 10
+
+            else:                                                                   # If not sleep
+
+                if device.height == 64:     # Only if 128 x 64 pixels
+                    for i in xrange(0, 128, 2):
+                        draw.point((i, 25), fill = 'white')
+                        draw.point((i, 40), fill = 'white')
+                        draw.text((0,26), u'\ue801', font = icon, fill = 'white')   # Icon stat
+
+                
+                if wake_up is True:
+                    draw.text((2, 0), u'\uf130', font = icon, fill = 'white')       # Icon talk
+                
+                if line[2][:4] == 'Last':                                           # Icon clock (DIY...)
+                    x = 6
+                    y = 17
+                    draw.ellipse((x - 6, y - 6, x + 6, y + 6), outline = 'white')
+                    draw.line((x, y, x + 2, y + 2), fill = 'white')
+                    draw.line((x, y, x, y - 3), fill = 'white')
+
+                # Print data
+
+                i = 0
+
+                for l in line:
+                    if l is not None:
+                        w, h = draw.textsize(text = l, font = font)
+                        tab = (device.width - w) / 2
+                        vide = ' ' * 22             # Hack to speed clear screen line... 
+                        draw.text((0, i), vide, font = font, fill = 'white')
+                        draw.text((tab, i), l, font = font, fill = 'white')
+                        i += h 
+                        if i == 24:
+                            if device.height != 64:  # Break if 128 x 32 pixels
+                                break
+                            i += 6
 
 
-            # Draw stats histogram
+                # Draw stats histogram
 
-            if device.height == 64:              # Only if 128 x 64 pixels
+                if device.height == 64:              # Only if 128 x 64 pixels
 
-                qso_hour_max = max(qso_hour)
+                    qso_hour_max = max(qso_hour)
 
-                i = 4
+                    i = 4
 
-                for q in qso_hour:
-                    if q != 0:
-                        h = interpolation(q, 1, qso_hour_max, 1, 15)
+                    for q in qso_hour:
+                        if q != 0:
+                            h = interpolation(q, 1, qso_hour_max, 1, 15)
+                        else:
+                            h = 0
+                        draw.rectangle((0 + i, 57, i + 2, (57 - 15)), fill = 'black')
+                        draw.rectangle((0 + i, 57, i + 2, (57 - h)), fill = 'white')
+                        i += 5
+
+                    legacy.text(draw,   (4, 59), chr(0) + chr(0), fill = 'white', font=SMALL_BITMAP_FONT)
+                    legacy.text(draw,  (32, 59), chr(0) + chr(6), fill = 'white', font=SMALL_BITMAP_FONT)
+                    legacy.text(draw,  (62, 59), chr(1) + chr(2), fill = 'white', font=SMALL_BITMAP_FONT)
+                    legacy.text(draw,  (92, 59), chr(1) + chr(8), fill = 'white', font=SMALL_BITMAP_FONT)
+                    legacy.text(draw, (115, 59), chr(2) + chr(3), fill = 'white', font=SMALL_BITMAP_FONT)
+
+            if blanc_alternate == 4:
+                # Print Room
+
+                i = 115
+
+                for c in room:
+                    legacy.text(draw,  (i, 1), chr(letter[c]), fill = 'white', font=SMALL_BITMAP_FONT)
+                    i += 4
+            else:
+                # Print Clock
+
+                i = 108
+
+                for c in now:
+                    if c == ':':
+                        c = 10
                     else:
-                        h = 0
-                    draw.rectangle((0 + i, 57, i + 2, (57 - 15)), fill = 'black')
-                    draw.rectangle((0 + i, 57, i + 2, (57 - h)), fill = 'white')
-                    i += 5
-
-                legacy.text(draw,   (4, 59), chr(0) + chr(0), fill = 'white', font=SMALL_BITMAP_FONT)
-                legacy.text(draw,  (32, 59), chr(0) + chr(6), fill = 'white', font=SMALL_BITMAP_FONT)
-                legacy.text(draw,  (62, 59), chr(1) + chr(2), fill = 'white', font=SMALL_BITMAP_FONT)
-                legacy.text(draw,  (92, 59), chr(1) + chr(8), fill = 'white', font=SMALL_BITMAP_FONT)
-                legacy.text(draw, (115, 59), chr(2) + chr(3), fill = 'white', font=SMALL_BITMAP_FONT)
-
-            # Print Clock
-
-            i = 108
-
-            for c in now:
-                if c == ':':
-                    c = 10
-                else:
-                    c = int(c)
-                legacy.text(draw,  (i, 0), chr(c), fill = 'white', font=SMALL_BITMAP_FONT)
-                i += 4
-
-            # Print Room
-
-            i = 115
-
-            for c in room:
-                legacy.text(draw,  (i, 6), chr(letter[c]), fill = 'white', font=SMALL_BITMAP_FONT)
-                i += 4
+                        c = int(c)
+                    legacy.text(draw,  (i, 1), chr(c), fill = 'white', font=SMALL_BITMAP_FONT)
+                    i += 4
 
 
         time.sleep(2)
