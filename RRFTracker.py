84aee5ee9b32b0733e45fa42fc05d5998696d12e
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-01-17 03:52:21 +0100

    Change condition on system info screen

--- RRFTracker.py
+++ RRFTracker.py
@@ -409,7 +409,7 @@ def main(argv):
                 if 'Waiting TX' not in call_time and len(history) >= 5 and device.height == 64:
                     extended = True
 
-            if wake_up is False and extended is True and minute % 2 == 0 and seconde < 20:          # System log extended
+            if wake_up is False and minute % 2 == 0 and seconde < 20:                               # System log extended
 
                 draw.rectangle((0, 0, 127, 63), fill='black')
 
