1209fc8d3608d5bb6d7b11d10d0f207b15f084e5
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-01-20 15:56:52 +0100

    Draw tot

--- display.py
+++ display.py
@@ -31,9 +31,9 @@ def tot(draw, legacy, start, current, font, position):
     print current, start, duration, h
 
     draw.rectangle((0, 54, 128, 44), fill='black')
-    for i in xrange(0, h, 3):
+    for i in xrange(0, h, 2):
         print i
-        draw.rectangle((i + 0, 54, i + 1, 44), fill='white')
+        draw.rectangle((i, 54, i, 44), fill='white')
     
     legacy.text(draw,   (0, position + 2), chr(0), fill='white', font=s.SMALL_BITMAP_FONT)
     
