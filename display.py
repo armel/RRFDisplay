69f1e09cb5809b5b32e9ff97ff7b5a7a6ee66a15
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-01-21 04:52:13 +0100

    Refactoring

--- display.py
+++ display.py
@@ -69,7 +69,11 @@ def tot(draw, legacy, start, current, position):
     msg = ''
     for c in tmp:
         msg += chr(int(c))
-    legacy.text(draw, (60, position + 2), msg, fill='white', font=s.SMALL_BITMAP_FONT)
+    if duration_max < 10:
+        tab = 4
+    else:
+        tab = 0
+    legacy.text(draw, (60 + tab, position + 2), msg, fill='white', font=s.SMALL_BITMAP_FONT)
 
 # Draw histogram
 def histogram(draw, legacy, position):
