fc8c2306fed9a1e63d52a5160ecef2bd72f63d96
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-01-26 18:01:51 +0100

    Remove dot zero distance

--- display.py
+++ display.py
@@ -125,6 +125,9 @@ def distance(draw):
         i = 0
         #d = str(d) + 'KM'
         d = str(d)
+        if '.0' in d:
+            d = d[:-2]
+
         for c in d:
             legacy.text(draw, (i, 18), chr(s.letter[c]), fill='white', font=s.SMALL_BITMAP_FONT)
             i += 4
