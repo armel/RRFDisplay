8d40a334a3a5de74aa7826d54a3a4edb383e8c5b
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-01-29 04:27:59 +0100

    Change version 2.4

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ Check video about RRFTracker on https://www.youtube.com/watch?v=rVW8xczVpEo
 
 # Version
 
-version = '2.3.2'
+version = '2.4.0'
 
 # Default i2c_port, i2c_address, display and room
 
