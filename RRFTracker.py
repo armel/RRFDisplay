6a2017d5d8bfcdc0875233ebe2ee83c1f7e39197
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-03-23 03:45:58 +0100

    Add log feature

--- RRFTracker.py
+++ RRFTracker.py
@@ -86,6 +86,8 @@ def main(argv):
         s.minute = int(s.now[3:-3])
         s.seconde = int(s.now[-2:])
 
+        l.log_write(s.log_path, s.day, s.qso_hour, s.history)
+
         if(s.now[:5] == '00:00'):
             if s.log is True:
                 l.log_write(s.log_path, s.day, s.qso_hour, s.history)
