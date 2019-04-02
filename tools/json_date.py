47e0ea7c51f85da3c0200cf17e7b5215255023f7
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-04-02 03:19:36 +0200

    Update stat

--- tools/json_date.py
+++ tools/json_date.py
@@ -6,7 +6,7 @@ filename = sys.argv[1]
 
 # Read log file
 log = [line.strip() for line in open(filename)]
-filename = '/var/www/log/' + os.path.basename(filename)
+filename = '/var/www/log/json/' + os.path.basename(filename)
 
 # Activity histogram
 data = '[\n'
