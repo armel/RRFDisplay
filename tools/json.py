99900d09fea95fba8d6694a64fd915b87b327210
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-04-03 23:35:27 +0200

    Update log data

--- tools/json.py
+++ tools/json.py
@@ -6,13 +6,23 @@ filename = sys.argv[1]
 
 # Read log file
 log = [line.strip() for line in open(filename)]
-filename = '/var/www/log/'
+filename = '/var/www/log/' + os.path.basename(filename) + '/'
+
+if not os.path.exists(filename):
+    os.makedirs(filename)
+    os.popen('cp /opt/RRFTracker_Spotnik/tools/index.html ' + filename + 'index.html')
 
 # Activity histogram
 data = '[\n'
 
+hour_max_when = ''
+hour_max_tx = 0
+
+hour_min_when = ''
+hour_min_tx = 1000
+
 l = 1
-for i in xrange(2, 2 + 24):
+for i in xrange(0, 24):
     x, y = log[i].split(' ')
 
     x = str('{:0>2d}'.format(int(x)))
@@ -30,6 +40,13 @@ for i in xrange(2, 2 + 24):
     data += '\t"TX": ' + y + '\n'
     data += '},\n'
 
+    if hour_max_tx < int(y):
+        hour_max_tx = int(y)
+        hour_max_when = x
+    if hour_min_tx > int(y):
+        hour_min_tx = int(y)
+        hour_min_when = x
+
 data += ']\n'
 
 last = data.rfind(',')
@@ -39,12 +56,31 @@ file = open(filename + 'activity.json', 'w')
 file.write(data)
 file.close()
 
+# Last link
+data = '[\n'
 
-# Best link
+for i in xrange(26, len(log)):
+    x = log[i]
+
+    data += '{\n'
+    data += '\t"Date": "' + x[0:8] + '",\n'
+    data += '\t"Call": ' + x[8:-1] + '\n'
+    data += '},\n'
+
+data += ']\n'
+
+last = data.rfind(',')
+data = data[:last] + '' + data[last + 1:]
+
+file = open(filename + 'last.json', 'w')
+file.write(data)
+file.close()
+
+# Best link: top 20
 data = '[\n'
 
 l = 1
-for i in xrange(27, len(log)):
+for i in xrange(32, len(log)):
     x = log[i].split(' ')
 
     data += '{\n'
@@ -65,11 +101,11 @@ file = open(filename + 'best.json', 'w')
 file.write(data)
 file.close()
 
-# Best link
+# Best link: all
 data = '[\n'
 
 l = 1
-for i in xrange(27, len(log)):
+for i in xrange(32, len(log)):
     x = log[i].split(' ')
 
     data += '{\n'
@@ -82,9 +118,28 @@ for i in xrange(27, len(log)):
 
 data += ']\n'
 
+call_total = l - 1
+
 last = data.rfind(',')
 data = data[:last] + '' + data[last + 1:]
 
 file = open(filename + 'all.json', 'w')
+file.write(data)
+file.close()
+
+# Activity abstract
+data = '[\n'
+
+data += '{\n'
+data += '\t"TX total": ' + str(log[0]) + ',\n'
+data += '\t"Noeuds actifs": ' + str(call_total) + '\n'
+data += '},\n'
+
+data += ']\n'
+
+last = data.rfind(',')
+data = data[:last] + '' + data[last + 1:]
+
+file = open(filename + 'abstract.json', 'w')
 file.write(data)
 file.close()
