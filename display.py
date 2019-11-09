2741341b549194ac85ac106515e71e26270a5939
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-09 19:34:24 +0100

    Fix bug

--- display.py
+++ display.py
@@ -385,12 +385,12 @@ def display_128():
             extended_system(draw, 3)
 
         # Call log extended
-        elif s.transmit is False and len(s.call_time) >=5 and s.minute % 2 == 0 and s.seconde < 45:
-            extended_call(draw, 10)
+        elif s.transmit is False and len(s.call) >=5 and s.minute % 2 == 0 and s.seconde < 45:
+            extended_call(draw, len(s.call))
 
         # Best log extended
-        elif s.transmit is False and len(s.history) >= 5 and s.minute % 2 == 0:
-            extended_best(draw, 10)
+        elif s.transmit is False and len(s.best) >= 5 and s.minute % 2 == 0:
+            extended_best(draw, len(s.best))
 
         # If not extended
         else:
