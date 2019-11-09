07778044a9ae92e030c575a3708dea7e5f1589e9
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-09 13:48:33 +0100

    Fix clear()

--- display.py
+++ display.py
@@ -138,8 +138,7 @@ def distance(draw):
 
 def extended_system(draw, page):
 
-    s.device.clear()
-    #draw.rectangle((0, 0, 127, 63), fill='black')
+    draw.rectangle((0, 0, 127, 63), fill='black')
 
     #draw.text((0, 0), u'\ue801', font=icon, fill='white')
 
