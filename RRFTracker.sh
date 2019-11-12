f9b4257791fd1866c9423ffb77a96494d6a07673
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-12 04:38:30 +0100

    Update shell script

--- RRFTracker.sh
+++ RRFTracker.sh
@@ -7,10 +7,10 @@ case "$1" in
     start)
         /bin/echo "Starting RRFTracker: I2C 0"
         cd $PATH
-        /usr/bin/nohup /usr/bin/python/python $PATH_SCRIPT --i2c-port 0 --display ssd1327 --display-height 128 --room SCAN --callsign F4HWN > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
+        /usr/bin/nohup /usr/bin/python $PATH_SCRIPT --i2c-port 0 --display ssd1327 --display-height 128 --room SCAN --callsign F4HWN > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
         /bin/echo "Starting RRFTracker: I2C 1"
         cd $PATH
-        /usr/bin/nohup /usr/bin/python/python $PATH_SCRIPT --i2c-port 1 --display ssd1327 --display-height 128 --room SCAN --callsign F1ZPX > $PATH_PID/RRFTracker_Spotnik_1.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_1.pid
+        /usr/bin/nohup /usr/bin/python $PATH_SCRIPT --i2c-port 1 --display ssd1327 --display-height 128 --room SCAN --callsign F1ZPX > $PATH_PID/RRFTracker_Spotnik_1.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_1.pid
         ;;
     stop) 
         /bin/echo "Stopping RRFTracker: I2C 0"
