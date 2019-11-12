c01c945558ed9b4c5ff70ce37f1076e253b801bc
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-12 04:34:33 +0100

    Update shell script

--- RRFTracker.sh
+++ RRFTracker.sh
@@ -7,10 +7,10 @@ case "$1" in
     start)
         echo "Starting RRFTracker: I2C 0"
         cd $PATH
-        nohup python $PATH_SCRIPT --i2c-port 0 --display ssd1327 --display-height 128 --room SCAN --callsign F4HWN > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
+        /usr/bin/nohup python $PATH_SCRIPT --i2c-port 0 --display ssd1327 --display-height 128 --room SCAN --callsign F4HWN > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
         echo "Starting RRFTracker: I2C 1"
         cd $PATH
-        nohup python $PATH_SCRIPT --i2c-port 1 --display ssd1327 --display-height 128 --room SCAN --callsign F1ZPX > $PATH_PID/RRFTracker_Spotnik_1.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_1.pid
+        /usr/bin/nohup python $PATH_SCRIPT --i2c-port 1 --display ssd1327 --display-height 128 --room SCAN --callsign F1ZPX > $PATH_PID/RRFTracker_Spotnik_1.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_1.pid
         ;;
     stop) 
         echo "Stopping RRFTracker: I2C 0"
