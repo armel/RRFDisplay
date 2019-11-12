31012af0ec0195da18fae112f3ad0edfc1bb7860
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-12 04:37:19 +0100

    Update shell script

--- RRFTracker.sh
+++ RRFTracker.sh
@@ -5,17 +5,17 @@ PATH_PID='/var/log'
 
 case "$1" in
     start)
-        echo "Starting RRFTracker: I2C 0"
+        /bin/echo "Starting RRFTracker: I2C 0"
         cd $PATH
-        /usr/bin/nohup python $PATH_SCRIPT --i2c-port 0 --display ssd1327 --display-height 128 --room SCAN --callsign F4HWN > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
-        echo "Starting RRFTracker: I2C 1"
+        /usr/bin/nohup /usr/bin/python/python $PATH_SCRIPT --i2c-port 0 --display ssd1327 --display-height 128 --room SCAN --callsign F4HWN > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
+        /bin/echo "Starting RRFTracker: I2C 1"
         cd $PATH
-        /usr/bin/nohup python $PATH_SCRIPT --i2c-port 1 --display ssd1327 --display-height 128 --room SCAN --callsign F1ZPX > $PATH_PID/RRFTracker_Spotnik_1.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_1.pid
+        /usr/bin/nohup /usr/bin/python/python $PATH_SCRIPT --i2c-port 1 --display ssd1327 --display-height 128 --room SCAN --callsign F1ZPX > $PATH_PID/RRFTracker_Spotnik_1.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_1.pid
         ;;
     stop) 
-        echo "Stopping RRFTracker: I2C 0"
-        kill `cat $PATH_PID/RRFTracker_Spotnik_0.pid`
-        echo "Stopping RRFTracker: I2C 1"
-        kill `cat $PATH_PID/RRFTracker_Spotnik_1.pid`
+        /bin/echo "Stopping RRFTracker: I2C 0"
+        /bin/kill `cat $PATH_PID/RRFTracker_Spotnik_0.pid`
+        /bin/echo "Stopping RRFTracker: I2C 1"
+        /bin/kill `cat $PATH_PID/RRFTracker_Spotnik_1.pid`
         ;;
 esac
