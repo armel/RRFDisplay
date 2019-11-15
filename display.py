391138e6f88bf9d0fe98d8fdba08541a408c794f
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-15 21:06:34 +0100

    Change GUI

--- display.py
+++ display.py
@@ -458,8 +458,8 @@ def display_128():
 
             # Icon clock (DIY...)
             if 'Dernier' in s.message[4]:
-                legacy.text(draw, (0, 8), chr(0) + chr(1), fill=s.color['white'], font=s.SMALL_BITMAP_CLOCK)
-                legacy.text(draw, (0, 16), chr(2) + chr(3), fill=s.color['white'], font=s.SMALL_BITMAP_CLOCK)
+                legacy.text(draw, (0, 0), chr(0) + chr(1), fill=s.color['white'], font=s.SMALL_BITMAP_CLOCK)
+                legacy.text(draw, (0, 8), chr(2) + chr(3), fill=s.color['white'], font=s.SMALL_BITMAP_CLOCK)
 
             # Icon stat
             draw.text((0, 0), u'\ue801', font=icon, fill=s.color['white'])
