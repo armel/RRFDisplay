d9bc6757c37118da6cadb6e85771d828682d85bc
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-15 07:04:44 +0100

    Change GUI

--- display.py
+++ display.py
@@ -496,7 +496,7 @@ def display_128():
                     draw.text((0, i), vide, font=font, fill=s.color['white'])
                     draw.text((tab, i), m, font=font, fill=color)
                     if j < 3:
-                        draw.text((18, i), str(j + 1) + '>', font=font, fill=color)
+                        draw.text((18, i), chr(1), font=s.SMALL_BITMAP_FONT, fill=color)
 
                     i += h
                     if i == 40:
