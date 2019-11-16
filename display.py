5f8b954aec915fffe06a04cf1b90d0111a29f796
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-16 03:48:19 +0100

    Change GUI

--- display.py
+++ display.py
@@ -470,6 +470,7 @@ def display_128():
                 #draw.text((0, 1), u'\ue805', font=icon, fill=s.color['white'])
                 #legacy.text(draw, (0, 1), chr(0) + chr(1), fill=s.color['white'], font=s.SMALL_BITMAP_STAT)
                 #legacy.text(draw, (0, 9), chr(2) + chr(3), fill=s.color['white'], font=s.SMALL_BITMAP_STAT)
+                a = 1
 
             # Icon talk
             if s.transmit is True:
