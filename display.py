0d55809e2ebfc57b5dc39d634b16bc552faae6a4
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-18 04:41:16 +0100

    Add config screen

--- display.py
+++ display.py
@@ -480,11 +480,11 @@ def display_64():
             extended_config(draw, 2)
 
         # Call log extended
-        elif s.transmit is False and len(s.call) >=5 and s.minute % 2 == 0 and s.seconde < 25:
+        elif s.transmit is False and len(s.call) >=5 and s.minute % 2 == 0 and s.seconde < 30:
             extended_call(draw, len(s.call))
 
         # Best log extended
-        elif s.transmit is False and len(s.best) >= 5 and s.minute % 2 == 0 and s.seconde < 30:
+        elif s.transmit is False and len(s.best) >= 5 and s.minute % 2 == 0 and s.seconde < 40:
             extended_best(draw, len(s.best))
 
         # If not extended
