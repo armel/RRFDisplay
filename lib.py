d82f9673f3f2c83d78fc0a81f050a764b0c0957f
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-11-22 02:46:33 +0100

    Fix contrast

--- lib.py
+++ lib.py
@@ -81,11 +81,11 @@ def save_stat(history, call):
 # Wake up screen
 def wake_up_screen(device, display, wake_up):
     if 'sh1106' in display:
-        sleep_level = 16
+        sleep_level = 8
     elif 'ssd1306' in display:
-        sleep_level = 16
+        sleep_level = 8
     else:
-        sleep_level = 16
+        sleep_level = 8
 
     if wake_up is True:
         for i in xrange(225, sleep_level, -1):
