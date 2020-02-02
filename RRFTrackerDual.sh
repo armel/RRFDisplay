8fa7b461d68beb899292b3cb5fd70ec95f2fa02c
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-02-02 01:16:24 +0100

    Update shell script

--- RRFTrackerDual.sh
+++ RRFTrackerDual.sh
@@ -6,9 +6,13 @@ case "$1" in
     start)
         echo "Starting RRFTracker: I2C 0"
         nohup python $PATH_SCRIPT --i2c-port 0 --display ssd1327 --display-height 128 --follow F1ZPX > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
+        echo "Starting RRFTracker: I2C 1"
+        nohup python $PATH_SCRIPT --i2c-port 1 --display ssd1327 --display-height 128 --follow F4HWN > $PATH_PID/RRFTracker_Spotnik_1.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_1.pid
         ;;
     stop) 
         echo "Stopping RRFTracker: I2C 0"
         kill `cat $PATH_PID/RRFTracker_Spotnik_0.pid`
+        echo "Stopping RRFTracker: I2C 1"
+        kill `cat $PATH_PID/RRFTracker_Spotnik_1.pid`
         ;;
 esac
