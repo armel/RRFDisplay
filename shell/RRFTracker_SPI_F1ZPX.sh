7e015b2ddedabd0ecaf6f21bc0f46a218239f43a
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-02-23 04:59:47 +0100

    Update theme

--- shell/RRFTracker_SPI_F1ZPX.sh
+++ shell/RRFTracker_SPI_F1ZPX.sh
@@ -7,7 +7,7 @@ cd ..
 case "$1" in
     start)
         echo "Starting RRFTracker: SPI"
-        nohup python $PATH_SCRIPT --interface spi --display ssd1351 --display-height 128 --display-theme theme_rgb.cfg --follow F1ZPX > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
+        nohup python $PATH_SCRIPT --interface spi --display ssd1351 --display-height 128 --display-theme theme_bleu.cfg --follow F1ZPX > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
         ;;
     stop) 
         echo "Stopping RRFTracker: SPI"
