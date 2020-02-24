e11c1dda4bb6348c5e9899153b48ebaaf68118ea
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-02-24 01:12:30 +0100

    Change theme semantic

--- RRFTracker_I2C.sh
+++ RRFTracker_I2C.sh
@@ -5,7 +5,7 @@ PATH_PID='/tmp'
 case "$1" in
     start)
         echo "Starting RRFTracker: I2C 0"
-        nohup python $PATH_SCRIPT --interface i2c --i2c-port 0 --display ssd1327 --display-height 128 --display-theme theme_gray.cfg --follow F1ZPX > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
+        nohup python $PATH_SCRIPT --interface i2c --i2c-port 0 --display ssd1327 --display-height 128 --display-theme gray.cfg --follow F1ZPX > $PATH_PID/RRFTracker_Spotnik_0.log 2>&1 & echo $! > $PATH_PID/RRFTracker_Spotnik_0.pid
         ;;
     stop) 
         echo "Stopping RRFTracker: I2C 0"
