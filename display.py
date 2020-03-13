035d8d2f9796a759ebdf12fe584571e7bb7b433d
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-03-13 04:17:23 +0100

    Fix bug with sh1106

--- display.py
+++ display.py
@@ -35,12 +35,14 @@ def label(draw, position, width, bg_color, fg_color, label, value, fixed = 0):
         position += 3
         draw.rectangle((0, position - 1, width, position + 8), fill=bg_color)
         draw.line((width + 1, position, width + 1, position + 7), fill=bg_color)
+        draw.line((width + 2, position + 1, width + 2, position + 6), fill=bg_color)
+        draw.line((width + 3, position + 2, width + 3, position + 5), fill=bg_color)
+        draw.line((width + 4, position + 3, width + 4, position + 4), fill=bg_color)
     else:
         draw.rectangle((0, position - 1, width, position + 7), fill=bg_color)
-
-    draw.line((width + 2, position + 1, width + 2, position + 6), fill=bg_color)
-    draw.line((width + 3, position + 2, width + 3, position + 5), fill=bg_color)
-    draw.line((width + 4, position + 3, width + 4, position + 4), fill=bg_color)
+        draw.line((width + 2, position + 1, width + 2, position + 6), fill=bg_color)
+        draw.line((width + 3, position + 2, width + 3, position + 5), fill=bg_color)
+        draw.line((width + 4, position + 3, width + 4, position + 4), fill=bg_color)
 
     draw.text((1, position), label, font=font, fill=fg_color)
     if fixed == 0:
