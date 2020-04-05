bbc50a2ea8f9d1ed2ac29060937bf7b267edef03
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-04-05 05:52:50 +0200

    Add cluster support

--- lib.py
+++ lib.py
@@ -464,8 +464,8 @@ def get_cluster():
     if cluster_data != '': # If valid stream
         limit = len(cluster_data)
         for item in xrange(0, limit):
-            if ['FT8', 'ft8', 'FT4', 'ft4'] not in cluster_data[item]['comment']:
-                print cluster_data[item]['call'] + ' ' + cluster_data[item]['freq'] + ' ' + cluster_data[item]['dxcall']
+            if ['FT8', 'ft8', 'FT4', 'ft4'] not in cluster_data[item][u'comment']:
+                print cluster_data[item][u'call'] + ' ' + cluster_data[item][u'freq'] + ' ' + cluster_data[item][u'dxcall']
         exit()
 
     return True  
