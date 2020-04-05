c0e2628b84f7e13cdb1f2b3fe1bf50841566226f
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-04-05 15:58:29 +0200

    Add cluster support

--- display.py
+++ display.py
@@ -534,8 +534,9 @@ def extended_cluster(draw, page):
         tmp = s.cluster_value[j].split(' ')
         label(draw, i, position, get_color('label', 'background'), get_color('label', 'foreground'), tmp[1], tmp[0])
         k = 108
-        print tmp
-        for c in tmp[3]:
+        time = tmp[4][0:5]
+        print time
+        for c in time:
             legacy.text(draw, (k, i), chr(s.letter[c]), fill=get_color('header', 'foreground'), font=s.SMALL_BITMAP_FONT)
             k += 4
 
