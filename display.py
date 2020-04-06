2c2b8c42f3040d7f279373a3f3928a1a054e029b
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-04-06 14:19:44 +0200

    Add cluster support

--- display.py
+++ display.py
@@ -668,14 +668,13 @@ def display_128():
                 extended_best(draw, len(s.best))
 
             # Propag extended
-            elif s.seconde < 40 and len(s.solar_value) != 0:
-                print 'ici'
+            elif s.seconde < 40:
                 extended_solar(draw, 3)
         
-            elif s.seconde < 48 and len(s.solar_value) != 0:
+            elif s.seconde < 48:
                 extended_solar(draw, 4)
             
-            elif s.seconde >= 48 and len(s.cluster_value) != 0:
+            elif s.seconde >= 48:
                 extended_cluster(draw, 1)
 
         # If not extended
