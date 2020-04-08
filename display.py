7fd1cd1e8ade214c285fc8c4f02b639335782e81
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-04-08 18:35:21 +0200

    Add title def

--- display.py
+++ display.py
@@ -759,7 +759,7 @@ def display_128():
 
                     w, h = draw.textsize(text=tmp, font=font_big)
                     tab = (s.device.width - w) / 2
-                    draw.text((tab, 14), tmp, font=font_big, fill=get_color('log', 'call_last'))
+                    draw.text((tab, 15), tmp, font=font_big, fill=get_color('log', 'call_last'))
 
                 # Draw tot
                 tot(draw, legacy, s.duration, 69)
