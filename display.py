f1df611e353fcbdcc5459f39c0a7a4607154fe5c
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-04-08 17:55:30 +0200

    Add title def

--- display.py
+++ display.py
@@ -120,7 +120,7 @@ def tot(draw, legacy, duration, position):
         tmp = l.convert_second_to_time(duration)
         w, h = draw.textsize(text=tmp, font=font_tot)
         tab = (s.device.width - w) / 2
-        draw.text((tab, 54), tmp, font=font_tot, fill=get_color('screen', 'foreground'))
+        draw.text((tab, 58), tmp, font=font_tot, fill=get_color('screen', 'foreground'))
 
 
 # Print elsewhere
