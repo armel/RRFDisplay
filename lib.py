b98ec5ad79b0bcdc5e5dcb65d287ee2fb8605d8f
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-01 23:08:19 +0200

    Move to Python3

--- lib.py
+++ lib.py
@@ -309,7 +309,7 @@ def scan(call):
                 try:
                     r = requests.get(s.room[q]['api'], verify=False, timeout=10)
                     page = r.content
-                    if call in page:
+                    if call.encode('utf8') in page:
                         return q
                 except requests.exceptions.ConnectionError as errc:
                     return False
