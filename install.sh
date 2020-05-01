37b2d777b0f91f23698473dbe1ab4709d18a538e
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-01 18:54:21 +0200

    Move to Python3

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-echo "Update"
+echo "Step 1 - Update"
 apt-get update
-echo "Some apt-get install"
+echo "Step 2 - Some apt-get install"
 apt-get -y install i2c-tools libi2c-dev python-smbus python-pip python-dev python-pil python-lxml
 apt-get -y install libfreetype6-dev libjpeg-dev build-essential
-echo "Some pip install"
+echo "Step 3 - Some pip install"
 pip3 install --upgrade setuptools
 pip3 install requests
 pip3 install configparser
 pip3 install luma.oled luma.lcd
-echo "Rock'n roll"
+echo "Step 4 - Rock'n roll"
