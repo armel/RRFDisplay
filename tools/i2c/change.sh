c1190dade95f91cccd0438cb65beede607b61353
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-02 23:32:15 +0200

    Move to Python3

--- tools/i2c/change.sh
+++ tools/i2c/change.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-cp /boot/dtb/sun8i-h2-plus-orangepi-zero.dtb .
-/usr/bin/dtc -I dts -O dtb ./sun8i-h2-plus-orangepi-zero.dts -o /boot/dtb/sun8i-h2-plus-orangepi-zero.dtb
+if [ -z "$1" ]
+	then
+    	vitesse="400k"
+    else
+    	vitesse=$1
+fi
+
+echo "Change i2C speed to $vitesse"
+/usr/bin/dtc -I dts -O dtb ./$vitesse.dts -o /boot/dtb/sun8i-h2-plus-orangepi-zero.dtb
+echo "Done !"
+echo "Please, reboot now !"
