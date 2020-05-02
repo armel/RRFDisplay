8e4b0ab835579e6158248feade4a6973e72cabd9
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-02 03:41:27 +0200

    Move to Python3

--- tools/i2c/change.sh
+++ tools/i2c/change.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cp /boot/dtb/sun8i-h2-plus-orangepi-zero.dtb ./boot/dtb/sun8i-h2-plus-orangepi-zero.dtb.backup
+cp /boot/dtb/sun8i-h2-plus-orangepi-zero.dtb .
 /usr/bin/dtc -I dts -O dtb ./sun8i-h2-plus-orangepi-zero.dts -o /boot/dtb/sun8i-h2-plus-orangepi-zero.dtb
