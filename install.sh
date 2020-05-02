e10bc935d8de621b25a64f4655778e4f23990afc
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-02 03:22:12 +0200

    Move to Python3

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ apt-get update
 echo "+---------------------------+"
 echo "Step 2 - Some apt-get install"
 echo "+---------------------------+"
-apt-get -y install i2c-tools libi2c-dev python-pip python-dev python3-lxml
+apt-get -y install i2c-tools libi2c-dev python-dev python3-lxml
 apt-get -y install libfreetype6-dev libjpeg-dev build-essential
 echo "+-----------------------+"
 echo "Step 3 - Some pip install"
