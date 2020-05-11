e8b4ae9a17dd96492a82ea497c298636363b81d6
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-11 23:25:26 +0200

    Add update

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 echo "+---------------------------+"
-echo "Step 1 - Some apt-get install"
+echo "Step 1 - Do apt-get update   "
+echo "+---------------------------+"
+apt-get update
+echo "+---------------------------+"
+echo "Step 2 - Some apt-get install"
 echo "+---------------------------+"
 apt-get -y install i2c-tools libi2c-dev python-dev python-lxml python3-lxml
 apt-get -y install libfreetype6-dev libjpeg-dev build-essential
 echo "+-----------------------+"
-echo "Step 2 - Some pip install"
+echo "Step 3 - Some pip install"
 echo "+-----------------------+"
 pip3 install --upgrade setuptools
 pip3 install requests
 pip3 install configparser
 pip3 install luma.oled luma.lcd
 echo "+------------------+"
-echo "Step 3 - Rock'n roll"
+echo "Step 4 - Rock'n roll"
 echo "+------------------+"
