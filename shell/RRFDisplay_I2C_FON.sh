f03f3e33bdaeb2ec475a1ad67faa50b3961cec0c
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-06-10 04:14:37 +0200

    Fix ssd1327

--- shell/RRFDisplay_I2C_FON.sh
+++ shell/RRFDisplay_I2C_FON.sh
@@ -7,7 +7,7 @@ cd ..
 case "$1" in
     start)
         echo "Starting RRFDisplay: I2C 1"
-        nohup python3 $PATH_SCRIPT --interface i2c --i2c-port 1 --display ssd1327 --display-height 128 --display-theme gray.cfg --follow FON > $PATH_PID/RRFDisplay_1.log 2>&1 & echo $! > $PATH_PID/RRFDisplay_1.pid
+        nohup python3 $PATH_SCRIPT --interface i2c --i2c-port 1 --display sh1106 --display-height 64 --display-theme mono.cfg --follow FON > $PATH_PID/RRFDisplay_1.log 2>&1 & echo $! > $PATH_PID/RRFDisplay_1.pid
         ;;
     stop) 
         echo "Stopping RRFDisplay: I2C 1"
