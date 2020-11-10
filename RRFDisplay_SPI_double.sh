bff092a43c51151c306c28c7ea875933b1c2a59f
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-10 17:05:49 +0100

    Refactoring SPI

--- RRFDisplay_SPI_double.sh
+++ RRFDisplay_SPI_double.sh
@@ -5,9 +5,9 @@ PATH_PID='/tmp'
 case "$1" in
     start)
         echo "Starting RRFDisplay: SPI 0"
-        echo python3 $PATH_SCRIPT --interface spi --display st7735 --spi-device 0 --display-width 160 --display-theme orange.cfg --follow F4HWN > $PATH_PID/RRFDisplay_0.log 2>&1 & echo $! > $PATH_PID/RRFDisplay_0.pid
+        nohup python3 $PATH_SCRIPT --interface spi --display st7735 --spi-device 0 --display-width 160 --display-theme orange.cfg --follow F4HWN > $PATH_PID/RRFDisplay_0.log 2>&1 & echo $! > $PATH_PID/RRFDisplay_0.pid
         echo "Starting RRFDisplay: SPI 1"
-        echo python3 $PATH_SCRIPT --interface spi --display st7735 --spi-device 1 --display-width 160 --display-theme blue.cfg --follow F1ZPX > $PATH_PID/RRFDisplay_1.log 2>&1 & echo $! > $PATH_PID/RRFDisplay_1.pid
+        nohup python3 $PATH_SCRIPT --interface spi --display st7735 --spi-device 1 --display-width 160 --display-theme blue.cfg --follow F1ZPX > $PATH_PID/RRFDisplay_1.log 2>&1 & echo $! > $PATH_PID/RRFDisplay_1.pid
         ;;
     stop) 
         echo "Stopping RRFDisplay SPI 0"
