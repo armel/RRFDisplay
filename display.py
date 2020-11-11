98b8aaf4c3ff13b24d344fbf93c15024304b1370
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-11 18:52:36 +0100

    Change theme

--- display.py
+++ display.py
@@ -624,7 +624,7 @@ def display_64():
 def display_128():
     with canvas(s.device, dither=True) as draw:
         #draw.rectangle((0, 0, 127, s.device.height - 1), fill=get_color('screen', 'background'))
-        draw.rectangle((0, 0, 127, s.device.height - 1), fill='black')
+        draw.rectangle((0, 0, 127, s.device.height - 1), fill='dimgray')
         draw.rectangle((0, 1, 127, 13), fill=get_color('header', 'background'))
 
         draw.line((0, 0, 127, 0), fill=get_color('header', 'border'))
