e5298ffbff6a50fe64a2562bac7a49e9c4ba4f2e
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-11 19:04:47 +0100

    Change theme

--- display.py
+++ display.py
@@ -520,6 +520,7 @@ def extended_solar(draw, page):
 def display_init(init_message):
     with canvas(s.device) as draw:
         print ('ici', s.device.width, s.device.height)
+        s.device.clear()
         draw.rectangle((0, 0, s.device.width - 1, s.device.height - 1), fill='black')
 
         position = 0
