6476bfdf3e670c582bce539f0f66ad0d34314c7d
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-12 00:11:26 +0100

    Change theme

--- colors/colors.py
+++ colors/colors.py
@@ -29,7 +29,7 @@ def main():
         time.sleep(5)
 
         # Cycle through some primary colours
-        for color in ["indianred", "lightcoral", "salmon", "darksalmon", "crimson", "red", "darked"]:
+        for color in ["indianred", "lightcoral", "salmon", "darksalmon", "crimson", "red", "deepink"]:
             with canvas(device, dither=True) as draw:
                 draw.rectangle(device.bounding_box, fill=color)
                 size = draw.textsize(color)
