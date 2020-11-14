1384f4c91a64636dd01a7d0a8a7f27f854453ed6
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-14 06:00:25 +0100

    Add Frame Buffer support

--- RRFDisplay.py
+++ RRFDisplay.py
@@ -281,10 +281,7 @@ def main(argv):
                     s.message[0] = 'Salon ' + s.room_current[:3]
 
         # Print screen
-        if s.device.height == 240:
-            print(s.device.height, s.device.width)
-            d.display_240()
-        elif s.device.height == 160:
+        if s.device.height == 160:
             d.display_128()
         elif s.device.height == 128:
             d.display_128()
